5c0f85e218d371df6ecd1701ac4c87c677969587
Asadullah Al Noman <76659134+noman62@users.noreply.github.com>
2024-07-28 00:02:42 +0600

    fix layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Footer from "../../components/Footer/Footer";
+import Gallery from "../../components/Gallery/Gallery";
 import Header from "../../components/Header/Header";
 import HostProfile from "../../components/HostProfile/HostProfile";
 import ListingOverview from "../../components/Listing Overview/ListingOverview";
@@ -9,19 +10,22 @@ import ReviewSection from "../../components/ReviewSection/ReviewSection";
 
 const Home = () => {
     return (
-        <div className="container">
-            <Header/>
+        <>
+            <Header />
             <div className="nav-divider"></div>
-            <ListingOverview/>
-            <hr />
-            <Map/>
-            <hr />
-            <ReviewSection/>
-            <hr/>
-            <HostProfile/>
-            <hr/>
-            <Footer/>
-        </div>
+            <div className="container">
+                <Gallery />
+                <ListingOverview />
+                <hr />
+                <Map />
+                <hr />
+                <ReviewSection />
+                <hr />
+                <HostProfile />
+                <hr />
+                <Footer />
+            </div>
+        </>
     );
 };
 
