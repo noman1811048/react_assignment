63c8eab26ee4a9245b050198ea82208bdc52b5d4
Asadullah Al Noman <76659134+noman62@users.noreply.github.com>
2024-07-28 23:34:14 +0600

    add design

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <Router>
         <Routes>
-        <Route path="/" element={<Home/>}>
+        <Route path="/hotel/:hotelSlug" element={<Home/>}>
         </Route>
       </Routes>
       
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -5,18 +5,32 @@ import image3 from '../../assets/images/image3.jpg';
 import image4 from '../../assets/images/image4.jpg';
 import image5 from '../../assets/images/imag5.jpg';
 
+
+
+
 const Gallery = () => {
     return (
-        <div className="image-grid">
-            <img src={image1} alt="Bedroom" className="main-image" />
-            <img src={image2} alt="Living room" />
-            <img src={image3} alt="Door lock" />
-            <img src={image4} alt="Dining area" />
-            <div className="image-container">
-                <img src={image5} alt="Kitchen" />
-                <div className="show-all">
-                    <div className="icon"></div>
-                    Show all photos
+        <div>
+            <div className="header">
+                <h1 className="title">Comfy New Apt. in Pueblo Libre!</h1>
+                <div className="actions">
+                    <button id="shareButton"><span className="share-icon">↗️</span> <span className="text">Share</span></button>
+                    <button id="heartButton" className="heart-button">
+                        <span className="heart-icon">♡</span> <span className="text">Save</span>
+                    </button>
+                </div>
+            </div>
+            <div className="image-grid">
+                <img src={image1} alt="Bedroom" className="main-image" />
+                <img src={image2} alt="Living room" />
+                <img src={image3} alt="Door lock" />
+                <img src={image4} alt="Dining area" />
+                <div className="image-container">
+                    <img src={image5} alt="Kitchen" />
+                    <div className="show-all">
+                        <div className="icon"></div>
+                        Show all photos
+                    </div>
                 </div>
             </div>
         </div>
